bbc4515447a9a477d8d288a1da86dae49e8b6b42
jessi <jessivilla657@gmail.com>
2025-12-08 21:47:07 -0600

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+import Blog from '../views/Blog.vue';
+import Contact from '../views/Contact.vue';
+import Portfolio from '../views/Contact.vue';
+import Services from '../views/Services.vue';
+import Team from '../views/Team.vue';
+
+const routes = [
+  { path: '/home', component: Home },
+  { path: '/blog', component: Blog },
+  { path: '/contact', component: Contact },
+  { path: '/portfolio', component: Portfolio },
+  { path: '/service', component: Services },
+  { path: '/team', component: Team },
+  { path: '/', redirect: '/Home' }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+});
+
+export default router;
